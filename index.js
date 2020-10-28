810cf1a408b23050a0ae3bf1b54ce7fe67847348
Warren Seah <no-reply@flagforex.com>
2020-10-28 16:14:02 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Require express and body-parser
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require('fs');
+const { json } = require("body-parser");
+const logs = require('./logs.json');
+
+// Initialize express and define a port
+const app = express();
+const PORT = 3000;
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+
+    // Create initial object
+    // const objArray = [];
+    // const timestamp = new Date();
+    // const obj = { dateTime: timestamp.toGMTString(), data: {} };
+    // objArray.push(obj);
+    // storeData(JSON.stringify(objArray), 'logs.json');
+    res.send('Hello World!').end();
+});
+
+app.post("/hook", (req, res) => {
+
+    // Read file and write to json 
+    try {
+        // console.log(req.body) // Call your action on the request here
+
+        const dataObj = JSON.parse(logs);
+        const timestamp = new Date();
+        const obj = { dateTime: timestamp.toGMTString(), data: req.body };
+        dataObj.push(obj);
+        // console.log(dataObj);
+
+        storeData( JSON.stringify(dataObj), 'logs.json');
+        res.status(200).end() // Responding is important
+    } catch (err) {
+        console.log('Read file error');
+        console.log(err);
+        res.status(500).end() // Responding is important
+    }
+
+});
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+const storeData = (data, path) => {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+        console.log('Write to file error');
+    }
+}
